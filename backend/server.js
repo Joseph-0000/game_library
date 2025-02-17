5a54888758f57aa3c8a226dd98d570f459e4e3e3
Joseph Salinas <140462648+Joseph-0000@users.noreply.github.com>
2025-02-18 05:32:15 +0800

    backend - create data

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Game from "./models/Game.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+app.use(express.json());
+
+app.post('/api/games', async (req, res) => {
+    const game = req.body;
+
+    if(!game.title || !game.genre || !game.platform || !game.release_year || !game.description || !game.image) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const newGame = new Game(game);
+    try {
+        await newGame.save();
+        res.status(201).json({ message: 'Game created successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error creating game' });
+    }
 })
 
 app.listen(3000, () => { 
